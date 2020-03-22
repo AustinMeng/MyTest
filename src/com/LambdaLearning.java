a76d2282bf8bd5b874a377bebff4f63b0f636a29
Austin12 <austin_meng@163.com>
2020-03-22 18:47:57 +0800

    add idea configuration

--- src/com/LambdaLearning.java
+++ src/com/LambdaLearning.java
@@ -8,30 +8,36 @@ interface MyFunctionalInterface {
     String sayHello();
 
 }
+
 @FunctionalInterface
-interface  MyFunctionalInterface1{
+interface MyFunctionalInterface1 {
     int incrementByFive(int a);
 }
+
 public class LambdaLearning {
     public static void main(String[] args) {
-        MyFunctionalInterface myFunctionalInterface = ()-> "Hello";
+        MyFunctionalInterface myFunctionalInterface = () -> "Hello";
         System.out.println(myFunctionalInterface.sayHello());
 
-        MyFunctionalInterface1 mfi = (x)->x+5;
+        MyFunctionalInterface1 mfi = (x) -> x + 5;
         System.out.println(mfi.incrementByFive(4));
 
         List<String> list = new ArrayList<>();
-        list.add("a");list.add("b");list.add("c");list.add("d");
-        list.forEach((name)->System.out.println(name));
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("d");
+        list.forEach((name) -> System.out.println(name));
 
-        MathOperation add = (a,b)->a+b;
-        System.out.println(add.operation(4,5));
+        MathOperation add = (a, b) -> a + b;
+        System.out.println(add.operation(4, 5));
 
-        MathOperation subtraction = (a,b)->a-b;
-        System.out.println(subtraction.operation(5,4));
+        MathOperation subtraction = (a, b) -> a - b;
+        System.out.println(subtraction.operation(5, 4));
 
     }
-    interface MathOperation{
+
+    interface MathOperation {
         int operation(int a, int b);
     }
 }
