899002ebd84f218f764ed6d93e0c264ca5515e70
AustinMeng <austin_meng@163.com>
2024-06-19 21:12:02 +1200

    Repository test

--- src/main/java/com/Test.java
+++ src/main/java/com/Test.java
@@ -1,5 +1,6 @@
 package com;
 
+import com.google.common.collect.Lists;
 import com.other.GrandpaClass;
 import com.other.ParentClass;
 import java.io.BufferedReader;
@@ -13,15 +14,18 @@ import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.apache.commons.lang3.StringUtils;
 
 public class Test {
 
   public static void main(String[] args) {
 
-    Object o1 = new Object();
-    Object o2 = o1;
-    Object o3 = o2;
-    System.out.println(o1 + " " + o2+ " " + o3);
+//    Object o1 = new Object();
+//    Object o2 = o1;
+//    Object o3 = o2;
+//    System.out.println(o1 + " " + o2+ " " + o3);
     //    Map<String, String> map = new LinkedHashMap<>();
 //
 //    map.put("a", "1");
@@ -56,27 +60,27 @@ public class Test {
 //    LocalDateTime now = LocalDateTime.now(ZoneId.of("Australia/Darwin"));
 //    System.out.println(now);
 
-    try (BufferedReader br = new BufferedReader(
-        new FileReader("path"))) {
-      br.readLine();
-    } catch (IOException e) {
-      e.printStackTrace();
-      System.out.println(e.getSuppressed().length);
-      //Arrays.stream(e.getSuppressed()).forEach(x-> System.out.println(x.getMessage()));
-    }
-
-    try (InputStream in = new FileInputStream("src");
-        OutputStream out = new FileOutputStream("dst")) {
-      byte[] buf = new byte[1024];
-      int n;
-      while ((n = in.read(buf)) >= 0) {
-        out.write(buf, 0, n);
-      }
-    } catch (FileNotFoundException e) {
-      e.printStackTrace();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+//    try (BufferedReader br = new BufferedReader(
+//        new FileReader("path"))) {
+//      br.readLine();
+//    } catch (IOException e) {
+//      e.printStackTrace();
+//      System.out.println(e.getSuppressed().length);
+//      //Arrays.stream(e.getSuppressed()).forEach(x-> System.out.println(x.getMessage()));
+//    }
+//
+//    try (InputStream in = new FileInputStream("src");
+//        OutputStream out = new FileOutputStream("dst")) {
+//      byte[] buf = new byte[1024];
+//      int n;
+//      while ((n = in.read(buf)) >= 0) {
+//        out.write(buf, 0, n);
+//      }
+//    } catch (FileNotFoundException e) {
+//      e.printStackTrace();
+//    } catch (IOException e) {
+//      e.printStackTrace();
+//    }
     Integer a = 1000, b = 1000;
     Integer m = 100, n = 100;
     System.out.println(a == b);
@@ -95,7 +99,7 @@ public class Test {
     superList.add(new ParentClass());
 
     //ChildClass childClass = extendList.get(0);
-    ParentClass parentClass = extendList.get(0);
+    //ParentClass parentClass = extendList.get(0);
     //ParentClass parentClass1 = superList.get(0);//superList can not be read directly, since not which
     //GrandpaClass grandpaClass = superList.get(0);
 
@@ -111,7 +115,31 @@ public class Test {
 
     Collections.synchronizedCollection(new ArrayList<>());
 
+    System.out.println(".....");
+    System.out.println("".toLowerCase());
 
+    String newPhone = "123-$456-78@90 89*";
+    String replacedPhone = replaceRegex(newPhone, "[^0-9\\-+ ]+", "");
+    System.out.println(replacedPhone); // 输出：123-456-7890
+
+    //System.out.println(StringUtils.appendIfMissing());
+
+    List<Integer> numbers = Lists.newArrayList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+    // 将列表按照大小为 3 进行分区
+    List<List<Integer>> partitionedLists = Lists.partition(numbers, 3);
+
+    // 打印分区后的列表
+    for (List<Integer> partition : partitionedLists) {
+      System.out.println(partition);
+    }
+
+    System.out.println(null == Boolean.TRUE);
   }
 
+  public static String replaceRegex(String input, String regex, String replacement) {
+    Pattern pattern = Pattern.compile(regex);
+    Matcher matcher = pattern.matcher(input);
+    return matcher.replaceAll(replacement);
+  }
 }
